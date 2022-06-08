ef4511a1fa2fd7816f5e2e8f1dee57f5745980c3
Viktor <viktor.m.rumenov@gmail.com>
2022-06-08 16:41:08 +0200

    Added deploy script for smart contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,47 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [signer1, signer2] = await ethers.getSigners();
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Bank = await ethers.getContractFactory("Bank", signer1);
+  const bank = await Bank.deploy();
+  const Avax = await ethers.getContractFactory("Avax", signer2);
+  const avax = await Avax.deploy();
+  const Dogecoin = await ethers.getContractFactory("Dogecoin", signer2);
+  const doge = await Dogecoin.deploy();
+  const ShibaInu = await ethers.getContractFactory("ShibaInu", signer2);
+  const shib = await ShibaInu.deploy();
+  await avax.deployed();
+  await bank.deployed();
+  await doge.deployed();
+  await shib.deployed();
 
-  await greeter.deployed();
+  await bank.whitelistToken(
+    ethers.utils.formatBytes32String("Avax"),
+    avax.address
+  );
 
-  console.log("Greeter deployed to:", greeter.address);
+  await bank.whitelistToken(
+    ethers.utils.formatBytes32String("Dogecoin"),
+    doge.address
+  );
+
+  await bank.whitelistToken(
+    ethers.utils.formatBytes32String("ShibaInu"),
+    shib.address
+  );
+
+  await bank.whitelistToken(
+    ethers.utils.formatBytes32String("Eth"),
+    "0x63D841Bc7814c3C8fB52b0D11A5E81D9DA17d27E"
+  );
+
+  console.log("Bank deployed to:", bank.address, "by", signer1.address);
+  console.log("Avax deployed to:", avax.address, "by", signer2.address);
+  console.log("Dogecoin deployed to:", doge.address, "by", signer2.address);
+  console.log("ShibaInu deployed to:", shib.address, "by", signer2.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
